2474437ad696c1b8c2c11f675d6485eb0b76cad1
abdullahehab <nrtroz.ae@gmail.com>
2024-05-27 20:30:07 +0300

    speech to text enhancement

--- lib/views/search/search_page.dart
+++ lib/views/search/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:lottie/lottie.dart';
 import 'package:miniso/helpers/theme.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
@@ -33,19 +34,21 @@ class _SearchPageState extends State<SearchPage> {
   void _startListening() async {
     await _speech.listen(
       onResult: (result) {
-        setState(() {
-          _recognizedText = result.recognizedWords;
-        });
+        _recognizedText = result.recognizedWords;
+        setState(() {});
       },
     );
-    setState(() {
-      _isListening = true;
-    });
+    _isListening = true;
+    setState(() {});
+    showBottomSheet();
   }
 
   void _stopListening() async {
     await _speech.stop();
     setState(() {});
+
+    if (!mounted) return;
+    Navigator.pop(context);
   }
 
   void _onSpeechResult(SpeechRecognitionResult result) {
@@ -54,55 +57,73 @@ class _SearchPageState extends State<SearchPage> {
     });
   }
 
+  Future<void> showBottomSheet() async {
+    await showModalBottomSheet(
+      context: context,
+      barrierColor: Colors.transparent,
+      builder: (context) {
+        return SizedBox(
+          height: 200,
+          width: double.infinity,
+          child: Lottie.asset(
+            'assets/animation/recording.json',
+            height: 300,
+            width: 300,
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: TextField(
-                decoration: InputDecoration(
-                  suffixIcon: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(onPressed: _startListening, icon: Icon(_isListening ? Icons.mic:Icons.mic_none)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      IconButton(
-                          onPressed: pickImage,
-                          icon: Icon(Icons.camera_alt_outlined)),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Icon(Icons.search),
-                      SizedBox(
-                        width: 5,
-                      ),
-                    ],
-                  ),
-                  labelText: 'Search',
-                  hintText: 'Accessories, bags, etc',
-                  helperText: 'supporting text',
-                  filled: true,
-                  fillColor: Colors.transparent,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: AppColors.searchBorder,
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(
-                        12,
-                      ),
-                    ),
-                  ),
+      appBar: AppBar(
+        flexibleSpace: Container(),
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Accessories, bags, etc',
+            hintStyle: const TextStyle(color: Colors.black),
+            border: InputBorder.none,
+            suffixIcon: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed:
+                      _speech.isNotListening ? _startListening : _stopListening,
+                  icon:
+                      Icon(_speech.isNotListening ? Icons.mic_off : Icons.mic),
                 ),
-              ),
+                const SizedBox(
+                  width: 5,
+                ),
+                IconButton(
+                    onPressed: pickImage,
+                    icon: const Icon(Icons.camera_alt_outlined)),
+                const SizedBox(
+                  width: 5,
+                ),
+                const Icon(Icons.search),
+                SizedBox(
+                  width: 5,
+                ),
+              ],
             ),
-          ],
+          ),
+          onChanged: (value) {
+            // Perform search functionality here
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(_recognizedText),
+            ],
+          ),
         ),
       ),
     );
